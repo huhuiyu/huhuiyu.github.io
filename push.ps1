1410a905a5bccc96470d7d7d841fb16e53b14329
huhuiyu <1069306849@qq.com>
2022-12-27 10:37:47 +0800

    推送脚本和多资源库

---
+++ push.ps1
@@ -0,0 +1,4 @@
+# github链接地址：git remote add github git@github.com:huhuiyu/huhuiyu.github.io.git
+# jihulab链接地址：git remote add jihulab git@jihulab.com:DarkKnight/huhuiyu.github.io.git
+git push
+git push jihulab
