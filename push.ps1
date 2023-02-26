8591a27d7f678eb792fc7f729fa5d8ef780dad98
huhuiyu <1069306849@qq.com>
2023-02-26 21:55:58 +0800

    添加gitee仓库

--- push.ps1
+++ push.ps1
@@ -1,4 +1,6 @@
 # github链接地址：git remote add github git@github.com:huhuiyu/huhuiyu.github.io.git
 # jihulab链接地址：git remote add jihulab git@jihulab.com:DarkKnight/huhuiyu.github.io.git
+# gitee链接地址：git remote add gitee git@gitee.com:huhuiyu/huhuiyu.github.io.git
 git push
 git push jihulab
+git push gitee
