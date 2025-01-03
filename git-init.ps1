0e98daccb9228f9be3bde5b5a3ce09c65b50f53f
huhuiyu <1069306849@qq.com>
2025-01-03 11:31:55 +0800

    多仓库脚本修改

---
+++ git-init.ps1
@@ -0,0 +1,6 @@
+# github仓库地址：git@github.com:huhuiyu/huhuiyu.github.io.git
+# 移除github仓库地址
+git remote remove gitee
+# 添加gitee和jihulab的仓库地址
+git remote add gitee git@gitee.com:huhuiyu/huhuiyu.github.io.git
+
--- push.ps1
+++ push.ps1
@@ -1,6 +1,7 @@
-# github链接地址：git remote add github git@github.com:huhuiyu/huhuiyu.github.io.git
-# jihulab链接地址：git remote add jihulab git@jihulab.com:DarkKnight/huhuiyu.github.io.git
-# gitee链接地址：git remote add gitee git@gitee.com:huhuiyu/huhuiyu.github.io.git
+# 多仓库推送
+git pull
+Write-Output "===========pull finish==========="
 git push
-git push jihulab
+Write-Output "===========push github finish==========="
 git push gitee
+Write-Output "===========push gitee finish==========="
